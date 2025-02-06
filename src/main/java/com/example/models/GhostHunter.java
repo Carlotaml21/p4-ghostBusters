26232fc279d70e4b23d0c077c1a406250b5b2bc8
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-06 14:24:37 +0100

    add testAddsNewGhostToTheList() (TDD:green)

--- src/main/java/com/example/models/GhostHunter.java
+++ src/main/java/com/example/models/GhostHunter.java
@@ -1,7 +1,17 @@
 package com.example.models;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class GhostHunter {
 
+    private List<Ghost> ghostContainer = new ArrayList<>();
 
+    public void captureGhost(Ghost ghost){
+        ghostContainer.add(ghost);
+    }
+
+    public List<Ghost> getGhostContainer(){
+        return ghostContainer;
+    }
 }
