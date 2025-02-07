7236b3c554c8823e70525067d2154e1726be78db
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-07 18:24:46 +0100

    added testReturnsTrappedGhost() (tdd:green)

--- src/main/java/com/example/controllers/GhostHunterController.java
+++ src/main/java/com/example/controllers/GhostHunterController.java
@@ -1,6 +1,21 @@
 package com.example.controllers;
 
+import java.util.List;
+
+import com.example.models.Ghost;
+import com.example.models.GhostHunter;
+
 public class GhostHunterController {
 
     
+    private GhostHunter hunter;
+
+    public GhostHunterController () {
+        this.hunter = new GhostHunter();
+    }
+
+    public List<Ghost> captureGhosts (String name, String ghostClass, String danger, String abilities, String captureDate) {
+        hunter.captureGhost(name, ghostClass, danger, abilities, captureDate);
+        return hunter.getGhostContainer();
+    }
 }
